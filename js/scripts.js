e787b415367b840b1fefb9df45255f8a669f3b68
jeeivan <jeeivansri@gmail.com>
2023-09-18 17:06:59 +0100

    Restarted project using grid and created aliens, ship and missile

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,150 @@
-
 // ! Variables and elements
-// ? Add SpaceShip
-// ? Add Alien
 // Board
+const board = document.querySelector(".board");
 
-
-const board = document.getElementById('board')
-const ctx = board.getContext('2d')
-
-
-board.width = 600
-board.height = 600
-
+const width = 11;
+const height = 10;
+const cellCount = width * height;
+let cells = [];
 
 // Spaceship
+const startingPosition = 104;
+let currentPosition = startingPosition;
 
-class Ship {
-    constructor() {
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+// Alien
+let aliens = []
+const alienPosition = 1;
 
-        const img = new Image()
-        img.src = './img/spaceship.png'
-        img.onload = () => {
-            this.image = img
-            this.width = 50
-            this.height = 50
-            this.position = {
-                x: board.width / 2 - this.width / 2,
-                y: board.height - 100
-            }
-    
-        }
-        
-    }
+// Missile
+class Missile {
+  constructor(position) {
+    this.position = position;
+    this.interval = null;
+  }
 
-    draw() {
-        if (this.image)
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-    }
+  move() {
+    this.interval = setInterval(() => {
+      // Remove missile from current position
+      this.removeMissile();
 
-    move() {
-        if (this.image) {
-            this.draw()
-            this.position.x += this.velocity.x
+      // Calculate new position
+      this.position -= width;
 
-            if (this.position.x < 0) {
-                this.position.x = 0;
-            } else if (this.position.x > board.width - this.width) {
-                this.position.x = board.width - this.width
-            }
-        }
-    }
+      // Check if missile reached the top
+      if (this.position < 0) {
+        this.removeMissile();
+        clearInterval(this.interval);
+      } else {
+        // Add missile to new position
+        this.addMissile();
+      }
+    }, 500); // Adjust the interval as per your preference
+  }
+
+  addMissile() {
+    cells[this.position].classList.add("missile");
+  }
+
+  removeMissile() {
+    cells[this.position].classList.remove("missile");
+  }
 }
-
-const ship = new Ship()
-ship.draw()
-
 
 // ! Functions
-function animate() {
-    requestAnimationFrame(animate)
-    ship.draw()
-    ship.move()
+// CREATE GRID CELLS
+function createBoard() {
+  // Use the cellCount to create our grid cells
+  for (let i = 0; i < cellCount; i++) {
+    // Create div cell
+    const cell = document.createElement("div");
+    // Add index to div element
+    cell.innerText = i;
+    // Add index to an attribute
+    // cell.dataset.index= i - another way of doing below
+    cell.setAttribute("data-index", i);
+    // Add the height and width to each grid cell (div)
+    cell.style.height = `${100 / height}%`;
+    cell.style.width = `${100 / width}%`;
+
+    // Add cell to grid
+    board.appendChild(cell);
+    // Add newly created cell to cells arrya
+    cells.push(cell);
+  }
+  // Add cat character class to starting position
+  addShip(startingPosition);
+  addAlien(alienPosition);
 }
 
-animate()
+// Adding Ship
+function addShip(position) {
+  console.log("Ship being added to the following cell ->", position);
+  cells[position].classList.add("ship");
+}
 
-function handleMovement(event) {
-    const key = event.keyCode
+// Removing Ship
+function removeShip() {
+  console.log("SHIP REMOVED");
+  cells[currentPosition].classList.remove("ship");
+}
 
-    const left = 37
-    const aLeft = 65
-    const right = 39
-    const dRight = 68
-
-    if (key === left || key === aLeft) {
-        console.log('Left')
-        ship.velocity.x = -1.5
-    } else if (key === right || key === dRight) {
-        console.log('Right')
-        ship.velocity.x = +1.5
-    } else {
-        console.log('INVALID KEY')
+// Adding Alien
+function addAlien(position) {
+  for (let i = 11; i < 43; i++) {
+    const col = i % width;
+    if (col !== 0 && col !== 1 && col !== width - 2 && col !== width - 1) {
+      console.log("Alien being added to the following cell ->", i);
+      cells[i].classList.add("alien");
+      aliens.push(i)
     }
+  }
+}
+
+// Moving Alien
+
+// Control Movement
+function handleMovement(event) {
+  const key = event.keyCode;
+  console.log(event.keyCode);
+
+  const left = 37;
+  const aLeft = 65;
+  const right = 39;
+  const dRight = 68;
+  const spacebar = 32;
+
+  // Remove ship from previous position before updating current position to new cell
+  removeShip();
+
+  console.log(currentPosition, width, currentPosition % width);
+
+  // Check which key was pressed and execute code
+  if (key === left || (key === aLeft && currentPosition % width !== 0)) {
+    console.log("LEFT");
+    currentPosition--;
+  } else if (
+    key === right ||
+    (key === dRight && currentPosition % width !== width - 1)
+  ) {
+    console.log("RIGHT");
+    currentPosition++;
+  } else if (key === spacebar) {
+    console.log("FIRE!");
+    const missile = new Missile(currentPosition - width);
+    missile.addMissile();
+    missile.move();
+  } else {
+    console.log("INVALID KEY");
+  }
+
+  // Add cat class once currentPosition has been updated
+  addShip(currentPosition);
 }
 
 // ! Events
-document.addEventListener('keyup', handleMovement)
+document.addEventListener("keyup", handleMovement);
 
 // ! Page Load
+createBoard();
+
+console.log(aliens);
