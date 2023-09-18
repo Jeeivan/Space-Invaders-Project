03cbbab555d7c7d04bc4c392e1183b4bdb83a6db
jeeivan <jeeivansri@gmail.com>
2023-09-18 11:29:14 +0100

    Added ship and functionality to move left/right

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,92 @@
 // ? Add SpaceShip
 // ? Add Alien
 // Board
-let rows = 16
-let columns = 16
-let tile = 32
+
 
 const board = document.getElementById('board')
 const ctx = board.getContext('2d')
-let boardWidth = tile * columns
-let boardHeight = tile * rows
+
+
+board.width = 600
+board.height = 600
+
 
 // Spaceship
-let shipHeight = tile
-let shipWidth = tile * 2
-let shipY = tile * rows
-let shipX = tile * columns/2
 
-let ship = {
-    width : shipWidth,
-    height : shipHeight,
-    y : shipY,
-    x : shipX
+class Ship {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const img = new Image()
+        img.src = './img/spaceship.png'
+        img.onload = () => {
+            this.image = img
+            this.width = 50
+            this.height = 50
+            this.position = {
+                x: board.width / 2 - this.width / 2,
+                y: board.height - 100
+            }
+    
+        }
+        
+    }
+
+    draw() {
+        if (this.image)
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+
+    move() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+
+            if (this.position.x < 0) {
+                this.position.x = 0;
+            } else if (this.position.x > board.width - this.width) {
+                this.position.x = board.width - this.width
+            }
+        }
+    }
 }
 
+const ship = new Ship()
+ship.draw()
+
+
 // ! Functions
+function animate() {
+    requestAnimationFrame(animate)
+    ship.draw()
+    ship.move()
+}
+
+animate()
+
+function handleMovement(event) {
+    const key = event.keyCode
+
+    const left = 37
+    const aLeft = 65
+    const right = 39
+    const dRight = 68
+
+    if (key === left || key === aLeft) {
+        console.log('Left')
+        ship.velocity.x = -1.5
+    } else if (key === right || key === dRight) {
+        console.log('Right')
+        ship.velocity.x = +1.5
+    } else {
+        console.log('INVALID KEY')
+    }
+}
 
 // ! Events
+document.addEventListener('keyup', handleMovement)
 
 // ! Page Load
