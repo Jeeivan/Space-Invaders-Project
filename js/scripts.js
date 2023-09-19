404b0f16c3acb4247ea4ccc28fb80b09e4a85d36
jeeivan <jeeivansri@gmail.com>
2023-09-19 12:59:21 +0100

    Added bomb to game and attempted to fix moving aliens function

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ let currentPosition = startingPosition;
 
 // Alien
 let aliens = [];
+let activeAliens = [];
 const alienPosition = 1;
 let alienDirection = 1;
 
@@ -62,6 +63,46 @@ class Missile {
       cells[this.position].classList.remove("alien");
       aliens.splice(alienIndex, 1);
       clearInterval(this.interval);
+      winGame();
+    }
+  }
+}
+
+// Alien Bombs
+class Bomb {
+  constructor(position) {
+    this.position = position;
+    this.interval = null;
+  }
+
+  move() {
+    this.interval = setInterval(() => {
+      this.removeBomb();
+      this.position += width;
+      if (this.position >= cellCount) {
+        this.removeBomb();
+        clearInterval(this.interval);
+      } else {
+        this.addBomb();
+        this.checkCollision();
+      }
+    }, 300); // Adjust the speed of the bomb
+  }
+
+  addBomb() {
+    cells[this.position].classList.add("bomb");
+  }
+
+  removeBomb() {
+    cells[this.position].classList.remove("bomb");
+  }
+
+  checkCollision() {
+    if (this.position === currentPosition) {
+      // Only trigger game over if bomb hits the ship
+      this.removeBomb();
+      cells[currentPosition].classList.remove("ship");
+      gameOver();
     }
   }
 }
@@ -90,7 +131,8 @@ function createBoard() {
   // Add cat character class to starting position
   addShip(startingPosition);
   addAlien(alienPosition);
-  setInterval(moveAlien, 500);
+  setInterval(moveAlien, 1000);
+  setInterval(dropRandomBomb, 1500);
 }
 
 // Adding Ship
@@ -113,6 +155,7 @@ function addAlien(position) {
       console.log("Alien being added to the following cell ->", i);
       cells[i].classList.add("alien");
       aliens.push(i);
+      activeAliens.push(i); // Add to activeAliens
     }
   }
 }
@@ -125,25 +168,33 @@ function removeAlien() {
 
 // Moving Alien
 function moveAlien() {
+  const leftEdge = activeAliens[0] % width === 0;
+  const rightEdge = activeAliens[activeAliens.length - 1] % width === width - 1;
+  console.log(activeAliens.length);
+  console.log(leftEdge);
+  console.log(rightEdge);
+  console.log(activeAliens);
   aliens.forEach((currentAlienPosition, index) => {
     // Remove alien from current position
     cells[currentAlienPosition].classList.remove("alien");
 
     // Calculate new position (move right by one cell)
-    const newAlienPosition = currentAlienPosition + alienDirection;
+    const newAlienPosition = currentAlienPosition + 1;
 
     // Check if alien reached the right border
-    if (newAlienPosition % width === width - 1 && alienDirection === 1) {
+    if (rightEdge) {
       // Move down and change direction to left
       const newDownPosition = newAlienPosition + width;
       cells[newDownPosition].classList.add("alien");
       aliens[index] = newDownPosition;
       alienDirection = -1;
-    } else if (newAlienPosition % width === 0 && alienDirection === -1) {
+      activeAliens = activeAliens.map((alien) => alien + 11);
+    } else if (leftEdge) {
       // Move down and change direction to right
       const newDownPosition = newAlienPosition + width;
       cells[newDownPosition].classList.add("alien");
       aliens[index] = newDownPosition;
+      alienDirection = 1;
     } else {
       // Add alien to new position
       cells[newAlienPosition].classList.add("alien");
@@ -192,13 +243,34 @@ function handleMovement(event) {
     console.log("INVALID KEY");
   }
 
-  // Add cat class once currentPosition has been updated
+  // Add ship class once currentPosition has been updated
   addShip(currentPosition);
+}
+
+// Get random alien index
+function getRandomAlienIndex() {
+  return Math.floor(Math.random() * aliens.length);
+}
+
+// Function to drop bombs from random aliens
+function dropRandomBomb() {
+  const randomAlienIndex = getRandomAlienIndex();
+  const randomAlienPosition = aliens[randomAlienIndex];
+  const bomb = new Bomb(randomAlienPosition);
+  bomb.addBomb();
+  bomb.move();
 }
 
 // Game Over Function
 function gameOver() {
   alert("Game Over! You were hit by an alien!");
+}
+
+// Win Game Function
+function winGame() {
+  if (activeAliens.length === 0) {
+    alert("Congrats! You have destroyed all aliens and saved Earth!");
+  }
 }
 
 // ! Events
